5cae35edab429f213de1d9ed9032d073db012d80
Hameedat Oyewopo <hameedattimileyinoyewopo@gmail.com>
2025-01-19 13:07:20 +0100

    final commit

--- basics.ts
+++ basics.ts
@@ -74,7 +74,28 @@ community = [
     }
 ]
 
+// function & types
 
+function add(a: number, b: number) {
+    return a + b;
+}
+
+function print(value: any) {
+    console.log(value);
+}
+
+//Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray =insertAtBeginning(demoArray, -1);
+
+const srtingArray = insertAtBeginning(['a', 'b', 'c'], 'd')
 
 
 
